b5d627b643687fe995a31eea1f0cefbdf1c35527
csh8696-git <csh8696@gmail.com>
2026-03-31 15:25:36 +0900

    add : initialize_project
    모듈 분리까지

---
+++ Source/MySpartaLog/MySpartaLog.Build.cs
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MySpartaLog: ModuleRules
+{
+	public MySpartaLog(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] 
+		{ 
+			"Core", "CoreUObject", "Engine", "InputCore", 
+		}
+		);
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+	}
+}
---
+++ Source/MySpartaLog/MySpartaLog.h
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+MYSPARTALOG_API DECLARE_LOG_CATEGORY_EXTERN(LogMySpartaModule, Log, All);
+
+class FMySpartaLog : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
